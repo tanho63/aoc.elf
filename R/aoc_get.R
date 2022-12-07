4a44acd7ef69545c447b3dc6e7946df01b3fcf99
mrcaseb <roemerle@gmail.com>
2022-12-07 14:18:17 +0100

    view problem site and return time invisibly

--- R/aoc_get.R
+++ R/aoc_get.R
@@ -27,8 +27,9 @@ aoc_get <- function(day, year = format(Sys.Date(),"%Y"), path = here::here(), ov
   input <- .aoc_get_input(day, year, aoc_cookie())
   .aoc_write_input(input, path, year, day, overwrite)
   cli::cli_process_done(msg = "Downloading input for {year}-{day} ... done! {Sys.time()}")
+  .aoc_view_problem(year = year, day = day)
 
-  return(Sys.time())
+  invisible(Sys.time())
 }
 
 .aoc_get_input <- function(day, year, cookie){
@@ -66,4 +67,16 @@ aoc_get <- function(day, year = format(Sys.Date(),"%Y"), path = here::here(), ov
   writeLines(input, con = input_path, sep = "")
   writeLines(format(Sys.time(),usetz = TRUE),
              file.path(path, year, glue::glue("day-{stringr::str_pad(day,2,'left',pad = '0')}-timestamp.txt")))
+}
+
+.aoc_view_problem <- function(year, day){
+  url <- paste("https://adventofcode.com", year, "day", day, sep = "/")
+
+  if (interactive()) {
+    cli::cli_alert_info("Opening {.url {url}}")
+    utils::browseURL(url)
+  } else {
+    cli::cli_alert_info("View problem at {.url {url}}")
+  }
+  invisible(url)
 }
